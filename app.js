817c20cb26d03c62815b5018c7fe1cc1793f9bd3
Danylo Baziewicz <danxbazi@gmail.com>
2025-01-20 11:05:19 -0300

    implementado funções para verificação, incersão e alteração de valores.

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function exibirTextoNaTela(tag, texto){ //função para exibição de texto, espera por uma tag (h1, p, etc.) e um texto para ser repassado.
+
+    let campo = document.querySelector(tag); //recebe e informa a tag (h1, p...)
+    campo.innerHTML = texto; //adiconado o texto recebido pela função na tag que foi repassada, seria mesma coisa que: h1 = 'olá mundo'
+    //innerHTML está sendo necessário para fazer a inserção dessas informações na tag
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto'); //texto exibido na tag h1
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10'); //texto exibido na tag p
 
 function verificarChute(){
-    console.log('ok');
+    let chute = document.querySelector('input').value
+    //console.log(chute == numeroSecreto); //comparação retornando true/false. Tipo booleano
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou'); //seleciona a tag h1 do HTML e na sequência atribuí o texto informado depois da virgula
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; //condicional ternário para singular ou plural das tentativas.
+        let mensagemTentativas = `Parabéns, você descobriu o número secreto com ${tentativas} ${palavraTentativa}`; //adicionado o número de tentativas e a palavra tentativa singular/plural através de variável.
+        exibirTextoNaTela('p', mensagemTentativas) //caso exista o acerto exibe a mensagem de quantas tentativas foram necessárias.
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if(chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+        exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
 }
